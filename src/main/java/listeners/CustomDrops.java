fbea0259bc2a27467b0d844e673f07ed41ef5be0
strad-dev <bqiang3.14@gmail.com>
2025-10-10 07:42:39 -0400

    slight tweaks to make both things work as best as possible

--- src/main/java/listeners/CustomDrops.java
+++ src/main/java/listeners/CustomDrops.java
@@ -899,6 +899,7 @@ public class CustomDrops implements Listener {
 			}
 		}
 
+		System.out.println(died.getScoreboardTags());
 		if(died.getScoreboardTags().contains("SkyblockBoss")) {
 			e.setDroppedExp(e.getDroppedExp() * 10);
 		} else if(p == null) {
