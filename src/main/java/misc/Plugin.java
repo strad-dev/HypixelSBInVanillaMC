0dc9e50807eef6622abb89099525f8c269f1c7c0
Stradivarius <bqiang3.14@gmail.com>
2025-10-29 09:40:34 -0400

    fix primal dragon desync when a threshold is triggered

--- src/main/java/misc/Plugin.java
+++ src/main/java/misc/Plugin.java
@@ -48,6 +48,7 @@ import java.util.Objects;
 @SuppressWarnings({"unused"})
 public class Plugin extends JavaPlugin {
 	private static Plugin instance;
+	private static UltimateAdvancementAPI advancementAPI;
 
 	@Override
 	public void onEnable() {
@@ -97,20 +98,9 @@ public class Plugin extends JavaPlugin {
 		getServer().addRecipe(AddRecipes.addEfficiency6Recipe(this));
 		getServer().addRecipe(AddRecipes.addFeatherFalling5Recipe(this));
 
-		UltimateAdvancementAPI api = UltimateAdvancementAPI.getInstance(this);
-
-		AdvancementTab tab = api.createAdvancementTab("skyblock");
-
-		RootAdvancement root = new RootAdvancement(tab, "root", new AdvancementDisplay(Material.NETHER_STAR, "SkyBlock", AdvancementFrameType.TASK, false, false, 0, 0, "Fully featured in 3-5 business days!"), "textures/block/light_blue_concrete.png");
-		BaseAdvancement advancement = new BaseAdvancement("defeat_wither_lords", new AdvancementDisplay(Material.NETHERITE_SWORD, "Slayer of Withers, Master of Worlds", AdvancementFrameType.CHALLENGE, true, true, 1, 0, "You are a mighty warrior."), root);
-		tab.registerAdvancements(root, advancement);
-
-		tab.getEventManager().register(tab, PlayerLoadingCompletedEvent.class, event -> {
-			tab.showTab(event.getPlayer());
-			tab.grantRootAdvancement(event.getPlayer());
-		});
-
 		getLogger().info("Started SkyBlock in Vanilla!");
+
+		setupAdvancements();
 
 		try {
 			Objects.requireNonNull(getServer().getScoreboardManager()).getMainScoreboard().registerNewObjective("Intelligence", Criteria.DUMMY, "Intelligence");
@@ -120,10 +110,41 @@ public class Plugin extends JavaPlugin {
 			getLogger().info("Deteced Intelligence.");
 		}
 
-		Utils.scheduleTask(this::passiveIntel, 100L);
+		Utils.scheduleTask(Plugin::passiveIntel, 100L);
 	}
 
-	public void passiveIntel() {
+	private static void setupAdvancements() {
+		advancementAPI = UltimateAdvancementAPI.getInstance(Plugin.getInstance());
+
+		AdvancementTab tab = advancementAPI.createAdvancementTab("skyblock");
+
+		RootAdvancement root = new RootAdvancement(tab, "root", new AdvancementDisplay(Material.NETHER_STAR, "SkyBlock", AdvancementFrameType.TASK, false, false, 0, 3, "Can you beat all the bosses?"), "textures/block/light_blue_concrete.png");
+		BaseAdvancement defeatChickzilla = new BaseAdvancement("defeat_chickzilla", new AdvancementDisplay(Material.COOKED_CHICKEN, "Cooked Chicken", AdvancementFrameType.TASK, true, true, 1, 6, "Yummy food."), root, 1);
+		BaseAdvancement defeatHardChickzilla = new BaseAdvancement("defeat_hard_chickzilla", new AdvancementDisplay(Material.CAMPFIRE, "Still Cooked", AdvancementFrameType.TASK, true, true, 2, 6, "Always a good day to have cooked chicken."), defeatChickzilla, 1);
+		BaseAdvancement defeatBroodfather = new BaseAdvancement("defeat_tarantula_broodfather", new AdvancementDisplay(Material.SPIDER_EYE, "Spider Squasher", AdvancementFrameType.TASK, true, true, 1, 5, "It will never know what hit it."), root, 1);
+		BaseAdvancement defeatPrimordialBroodfather = new BaseAdvancement("defeat_primordial_broodfather", new AdvancementDisplay(Material.FERMENTED_SPIDER_EYE, "Saving Austrailia", AdvancementFrameType.CHALLENGE, true, true, 2, 5, "One less scary spider."), defeatBroodfather, 1);
+		BaseAdvancement defeatmeloGnorI = new BaseAdvancement("defeat_melog_nori", new AdvancementDisplay(Material.WRITABLE_BOOK, "Master's in Antimatter", AdvancementFrameType.TASK, true, true, 1, 4, "Granted by the University of SkyBlock."), root, 1);
+		BaseAdvancement defeatHardmeloGnorI = new BaseAdvancement("defeat_hard_melog_nori", new AdvancementDisplay(Material.ENCHANTED_BOOK, "Doctorate in Antimatter", AdvancementFrameType.TASK, true, true, 2, 4, "Also granted by the University of SkyBlock."), defeatmeloGnorI, 1);
+		BaseAdvancement defeatWitherLords = new BaseAdvancement("defeat_wither_lords", new AdvancementDisplay(Material.WITHER_SKELETON_SKULL, "Slayer of Withers, Master of Worlds", AdvancementFrameType.CHALLENGE, true, true, 1, 3, "You are a mighty warrior."), root, 1);
+		BaseAdvancement defeatRevenant = new BaseAdvancement("defeat_revenant_horror", new AdvancementDisplay(Material.ROTTEN_FLESH, "Trolling the Reaper", AdvancementFrameType.TASK, true, true, 1, 2, "Granted by the University of SkyBlock."), root, 1);
+		BaseAdvancement defeatAtonedHorror = new BaseAdvancement("defeat_atoned_horror", new AdvancementDisplay(Material.ZOMBIE_HEAD, "The Dead Truly Die", AdvancementFrameType.TASK, true, true, 2, 2, "Also granted by the University of SkyBlock."), defeatRevenant, 1);
+		BaseAdvancement defeatSadan = new BaseAdvancement("defeat_melog_nori", new AdvancementDisplay(Material.WRITABLE_BOOK, "The Apprentice Necromancer", AdvancementFrameType.TASK, true, true, 3, 2, "Granted by the University of SkyBlock."), defeatAtonedHorror, 1);
+		BaseAdvancement defeatHardSadan = new BaseAdvancement("defeat_hard_melog_nori", new AdvancementDisplay(Material.ENCHANTED_BOOK, "The Master Necromancer", AdvancementFrameType.CHALLENGE, true, true, 4, 2, "Also granted by the University of SkyBlock."), defeatSadan, 1);
+		BaseAdvancement defeatZealot = new BaseAdvancement("defeat_zealot", new AdvancementDisplay(Material.ENDER_PEARL, "The End?", AdvancementFrameType.TASK, true, true, 1, 1, "???"), root, 1);
+		BaseAdvancement defeatBrusier = new BaseAdvancement("defeat_zealot_brusier", new AdvancementDisplay(Material.ENDER_EYE, "The End.", AdvancementFrameType.TASK, true, true, 2, 1, "Or is it?"), defeatZealot, 1);
+		BaseAdvancement defeatVoidgloom = new BaseAdvancement("defeat_voidgloom_seraph", new AdvancementDisplay(Material.END_CRYSTAL, "Ender of Ender", AdvancementFrameType.TASK, true, true, 3, 1.5f, "Silence falls across the land."), defeatBrusier, 1);
+		BaseAdvancement defeatVoidcrazedSeraph = new BaseAdvancement("defeat_voidcrazed_seraph", new AdvancementDisplay(Material.BEACON, "The Line Between Genius and Insanity", AdvancementFrameType.CHALLENGE, true, true, 4, 1.5f, "??????????!!!!!!!!!!"), defeatVoidgloom, 1);
+		BaseAdvancement defeatPrimalDragon = new BaseAdvancement("defeat_primal_dragon", new AdvancementDisplay(Material.DRAGON_EGG, "The Beginning.", AdvancementFrameType.CHALLENGE, true, true, 3, 0.5f, "The start of something new."), defeatBrusier, 1);
+
+		tab.registerAdvancements(root, defeatChickzilla, defeatHardChickzilla, defeatBroodfather, defeatPrimordialBroodfather, defeatmeloGnorI, defeatHardmeloGnorI, defeatWitherLords, defeatRevenant, defeatAtonedHorror, defeatSadan, defeatHardSadan, defeatZealot, defeatBrusier, defeatVoidgloom, defeatVoidcrazedSeraph, defeatPrimalDragon);
+
+		tab.getEventManager().register(tab, PlayerLoadingCompletedEvent.class, event -> {
+			tab.showTab(event.getPlayer());
+			tab.grantRootAdvancement(event.getPlayer());
+		});
+	}
+
+	public static void passiveIntel() {
 		for(Player p : Bukkit.getServer().getOnlinePlayers()) {
 			try {
 				Score score = Objects.requireNonNull(Objects.requireNonNull(Plugin.getInstance().getServer().getScoreboardManager()).getMainScoreboard().getObjective("Intelligence")).getScore(p.getName());
@@ -139,7 +160,7 @@ public class Plugin extends JavaPlugin {
 				return;
 			}
 		}
-		Utils.scheduleTask(this::passiveIntel, 100L);
+		Utils.scheduleTask(Plugin::passiveIntel, 100L);
 	}
 
 	@Override
@@ -149,5 +170,9 @@ public class Plugin extends JavaPlugin {
 
 	public static Plugin getInstance() {
 		return instance;
+	}
+
+	public static UltimateAdvancementAPI getAdvancementAPI() {
+		return advancementAPI;
 	}
 }
