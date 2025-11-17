c1bfd2b690d2db51091ad7ca40d960ff721c4525
Stradivarius <bqiang3.14@gmail.com>
2025-11-17 18:19:30 -0500

    add handling for mutant giant and falling blocks

---
+++ src/main/java/mobs/hardmode/generic/MutantGiant.java
@@ -0,0 +1,23 @@
+package mobs.hardmode.generic;
+
+import listeners.DamageType;
+import misc.DamageData;
+import mobs.CustomMob;
+import org.bukkit.entity.*;
+
+public class MutantGiant implements CustomMob {
+	@Override
+	public String onSpawn(Player p, Mob e) {
+		return "";
+	}
+
+	@Override
+	public boolean whenDamaged(LivingEntity damagee, Entity damager, double originalDamage, DamageType type, DamageData data) {
+		return !(damagee instanceof FallingBlock);
+	}
+
+	@Override
+	public boolean whenDamaging(LivingEntity damagee, Entity damager, double originalDamage, DamageType type, DamageData data) {
+		return true;
+	}
+}
