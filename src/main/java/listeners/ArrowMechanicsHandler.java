9b2a2a0572c3e288fb71d518799eeaad8dc4f990
Stradivarius <bqiang3.14@gmail.com>
2026-01-02 11:47:31 -0800

    fix slight bugs with bonzo staff

--- src/main/java/listeners/ArrowMechanicsHandler.java
+++ src/main/java/listeners/ArrowMechanicsHandler.java
@@ -1,13 +1,33 @@
 package listeners;
 
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.v1_21_R7.entity.CraftPlayer;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityKnockbackByEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.util.Vector;
 
 public class ArrowMechanicsHandler implements Listener {
+
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent e) {
+		if(e.getEntity() instanceof WindCharge windCharge && windCharge.getScoreboardTags().contains("Bonzo")) {
+			e.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onEntityKnockbackByEntity(EntityKnockbackByEntityEvent e) {
+		if(e.getSourceEntity() instanceof WindCharge windCharge && windCharge.getScoreboardTags().contains("Bonzo")) {
+			e.setCancelled(true);
+		}
+	}
 
 	@EventHandler
 	public void onProjectileHit(ProjectileHitEvent e) {
@@ -29,21 +49,28 @@ public class ArrowMechanicsHandler implements Listener {
 
 			if(windCharge.getShooter() instanceof Player p) {
 				double distance = p.getLocation().distanceSquared(windCharge.getLocation());
+				p.getWorld().spawnParticle(Particle.TOTEM_OF_UNDYING, windCharge.getLocation(), 350, 0, 0, 0, 0.75);
+				p.getWorld().spawnParticle(Particle.CRIT, p.getLocation(), 150, 0, 0, 0, 2);
+				p.getWorld().playSound(windCharge.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 2.0F, 1.0F);
 
 				if(distance <= 16) {
-					e.setCancelled(true);
-					windCharge.remove();
+					if(!(p instanceof CraftPlayer craftPlayer)) return;
+					ServerPlayer serverPlayer = craftPlayer.getHandle();
 
-					// Calculate direction vector from wind charge to shooter
 					Vector direction = p.getLocation().toVector().subtract(windCharge.getLocation().toVector()).normalize();
-
-					// Scale to 1.52552 on X/Z plane with Y at 0.5
 					direction.setY(0);
 					direction.normalize();
 					direction.multiply(1.52552);
 					direction.setY(0.5);
 
-					// Apply velocity to the shooter
+					if(!Double.isFinite(direction.getX())) {
+						direction.setX(0);
+					}
+					if(!Double.isFinite(direction.getZ())) {
+						direction.setZ(0);
+					}
+
+					serverPlayer.setOnGround(false);
 					p.setVelocity(direction);
 				}
 			}
