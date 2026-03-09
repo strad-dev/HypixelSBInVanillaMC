10fd9119bb7500858237d4217455d721a96f2560
Stradivarius <bqiang3.14@gmail.com>
2026-03-09 13:42:44 -0400

    DiscordSRV support

---
+++ src/main/java/listeners/DiscordForwarder.java
@@ -0,0 +1,11 @@
+package listeners;
+
+import github.scarsz.discordsrv.DiscordSRV;
+import org.bukkit.entity.Player;
+
+public class DiscordForwarder {
+	@SuppressWarnings("deprecation")
+	public static void forward(Player player, String message) {
+		DiscordSRV.getPlugin().processChatMessage(player, message, "global", false);
+	}
+}
