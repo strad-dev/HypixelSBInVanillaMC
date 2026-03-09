724dcbc21409e64966deb782f1400408a01033e4
Stradivarius <bqiang3.14@gmail.com>
2026-03-09 15:56:23 -0400

    recolor action item

--- src/main/java/listeners/CustomDamage.java
+++ src/main/java/listeners/CustomDamage.java
@@ -1107,7 +1107,7 @@ public class CustomDamage implements Listener {
 									score.setScore(score.getScore() + 1);
 									p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy(ChatColor.AQUA + "Intelligence: " + score.getScore() + "/2500"));
 								} else {
-									p.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy(ChatColor.AQUA + "Intelligence: " + score.getScore() + "/2500 " + ChatColor.RED + ChatColor.BOLD + "MAX INTELLIGENCE"));
+									p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(TextComponent.fromLegacyText(ChatColor.AQUA + "Intelligence: " + score.getScore() + "/2500 " + ChatColor.RED + ChatColor.BOLD + "MAX INTELLIGENCE")));
 								}
 							} catch(Exception exception) {
 								Plugin.getInstance().getLogger().info("Could not find Intelligence objective!  Please do not delete the objective - it breaks the plugin");
