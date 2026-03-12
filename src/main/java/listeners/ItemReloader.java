df881c4034d5442812c4ebb4460fafcdac79e9ff
Stradivarius <bqiang3.14@gmail.com>
2026-03-12 00:08:47 -0400

    idk

--- src/main/java/listeners/ItemReloader.java
+++ src/main/java/listeners/ItemReloader.java
@@ -9,11 +9,13 @@ import items.summonItems.*;
 import items.weapons.Claymore;
 import items.weapons.Scylla;
 import items.weapons.Terminator;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -22,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 
 public class ItemReloader implements Listener {
-
 	@EventHandler
 	public void onItemPickup(EntityPickupItemEvent e) {
 		if (!(e.getEntity() instanceof Player p)) return;
